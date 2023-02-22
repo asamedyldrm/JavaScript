2e2514fe73f74097a5e7f9065edf8e8a562ad244
Samet Yıldırım <smtyldrm32@gmail.com>
2023-02-23 02:46:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,9 +18,70 @@
 //     console.log("i değeri: " + i)
 // }
 
-var i = 0;
+// var i = 0;
 
-while (i<50){
-    console.log(i)
-    i +=2;
+// while (i<50){
+//     console.log(i)
+//     i +=2;
+// }
+
+// var sayilar = [32, 5, 100, -1, 85]
+
+// console.log("Array Elemanları: ")
+
+// for (var i = 0; i<sayilar.length; i++){
+//     console.log("Eleman:" + sayilar[i])
+// }
+
+//Pratik Yöntemi
+// sayilar.forEach(function(sayi){
+//     console.log("Eleman: " + sayi)
+// })
+
+
+// var a = 5
+// var b = 6
+
+// console.log(a==b)
+
+// if (a != b){
+//     console.log("Eşit Değil")
+// }
+
+// if (10 < 20){
+//     console.log("Küçüktür.")
+// }
+
+if (2 === "2") {
+    console.log("Eşit")
 }
+
+if (2 == "2") {
+    console.log("Eşit")
+}
+
+if (2 == 2 && "Samet" == "Samet") {
+    console.log("Ve operatörü")
+}
+
+if (2 == 2 || "Murat" == "Osman") {
+    console.log("Or operatörü")
+}
+
+if (! (5<4)){
+    console.log("Not Operatörü")
+}
+
+var a = 10;
+var b = 20;
+
+//if and else
+
+if (a==b){
+    console.log("Eşit")
+}
+
+else {
+    console.log("Eşit Değil.")
+}
+
